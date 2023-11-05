1565b9fcfc94d76fb7956fdc33d98e0674dfae22
GREGORIOtsr <gralfonsotr@gmail.com>
2023-11-05 12:26:46 +0100

    added hellow world

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const port = 3000;
 // Routes
 app.use('/api/film', filmsRoutes);
 
+app.get('/', (req, res) => {
+  res.status(200).json({ msg: "Hello World" });
+})
+
 app.listen(port, () => {
   console.log(`> Listening on http://localhost:${port}`);
 });
